c864e656c8c8701f98ce0d86d42663dc600f813c
Mateusz <Mateusz@LAPTOP-O1KSSE5I>
2018-04-12 17:25:39 +0200

    działające wczytywanie plików

--- GrupProj/src/main/App.java
+++ GrupProj/src/main/App.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.io.*;
+import java.util.*;
 /**
  * Ciało głównej aplikacji
  *
@@ -13,7 +15,33 @@ package main;
  *
  */
 public class App {
-    public static void main(String[] args) {
+private final static String KATALOG_WEJSCIOWY = "electron\\ch1_org_filtered";
+    
+    public static void main(String[] args) throws Exception
+    {
+        File katalog;     // katalog z plikami
+        File[] pliki;     // tablica plikow
+        OpenFile dane;    // dane z pojedynczego pliku
+        ArrayList<OpenFile> kolekcjaDanych;    // dane z wszystkich plikow
 
+        katalog = new File(KATALOG_WEJSCIOWY);    // tworze katalog
+        pliki = katalog.listFiles();    // pobieram pliki z katalogu
+        kolekcjaDanych = new ArrayList<>();    // lista na dane
+        
+        for (File plik : pliki)    // petla po plikach
+        {
+        	// sciezka + nazwa pliku, ktory obecnie wczytuje
+            System.out.println("Trwa wczytywanie: " + plik.getAbsolutePath());
+            
+            dane = new OpenFile();    // tworze obiekt z danymi
+            dane.wczytaj(plik.getAbsolutePath());    // wczytuje dane
+
+            dane.pobierzOryginalne();
+            dane.pobierzBessel60();
+            dane.pobierzBessel80();
+            
+            kolekcjaDanych.add(dane);    // dodaje dane do kolekcji
+            System.out.println("[OK]");
+        }
     }
 }
--- GrupProj/src/main/Tools/OpenFile.java
+++ GrupProj/src/main/Tools/OpenFile.java
@@ -8,4 +8,50 @@ package main.Tools;
  * - zrzut danych do listy obiektów typu DataFrame
  */
 public class OpenFile {
+	
+	private Double[] oryginalne;
+    private Double[] bessel60;
+    private Double[] bessel80;
+    
+    public void wczytaj(String sciezka) throws FileNotFoundException, IOException
+    {
+        BufferedReader plik;
+        
+        plik = new BufferedReader(new FileReader(sciezka));
+        plik.readLine();    // pomijam zawsze 1-szy wiersz
+        oryginalne = konwertujWierszNaTablice(plik);    // 2-gi wiersz
+        bessel60 = konwertujWierszNaTablice(plik);    // 3-ci wiersz
+        bessel80 = konwertujWierszNaTablice(plik);    // 4-ty wiersz
+    }
+    
+    public Double[] pobierzOryginalne() { return oryginalne; }
+    public Double[] pobierzBessel60() { return bessel60; }
+    public Double[] pobierzBessel80() { return bessel80; }
+    
+    private Double[] konwertujWierszNaTablice(BufferedReader plik) throws IOException
+    {
+        ArrayList<Double> lista;
+        Double[] tablica;
+        Double wartosc;
+        String linia;
+        String[] eksplozja;
+        
+        linia = plik.readLine();
+        eksplozja = linia.split("\\s+", -1);
+        lista = new ArrayList<>();
+      
+        for (String tekst : eksplozja)
+        {
+            if (!tekst.equals(""))
+            {
+                wartosc = Double.parseDouble(tekst);    // pojedyncza wartosc
+                lista.add(wartosc);    // dodaje do listy
+            }
+        }
+        
+        tablica = new Double[lista.size()];
+        
+        return lista.toArray(tablica);
+    }
+	
 }
